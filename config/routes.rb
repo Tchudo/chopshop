14db340c89cdc7dc6b34536f627e435635caa349
Tchudo <gael.lebreton@gmail.com>
2020-02-24 17:57:23 +0100

    creating user

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'searches/new'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'favorites/index'
-  get 'favorites/create'
-  get 'stocks/index'
-  get 'stocks/show'
-  get 'search/new'
+
   devise_for :users
-  root to: 'search#new'
+  root to: 'searches#new'
 
   resources :stocks, only:[:index, :show], shallow: true do
     resources :favorites, only:[:index, :create], shallow:true
