846df7579dee259d5f65c577baf7f5f072773c7b
Tchudo <gael.lebreton@gmail.com>
2020-02-24 20:54:40 +0100

    idem 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get "/searches", to: 'searches#new'
+  root to: 'searches#new'
+
 
   resources :stocks, only:[:index, :show], shallow: true do
     resources :favorites, only:[:index, :create], shallow:true
     resources :reviews, only:[:new, :create], shallow:true
   end
-
-
 end
