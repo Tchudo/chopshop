5e91b5facda1f8a4fd6c68781fdaf8c17580fab9
Antoine Koenig <antoine24240@gmail.com>
2020-02-25 14:30:50 +0100

    Geocoding ok

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,9 @@ class Shop < ApplicationRecord
   has_many :time_tables
   has_many :products, through: :stocks
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   # validates address, presence: true
   # validates user_id, presence: true
 end
