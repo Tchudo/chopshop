ea50699e713d82258a0e36d1e7673471ff02c7ab
Tchudo <gael.lebreton@gmail.com>
2020-02-25 14:56:34 +0100

    param de cloudinary

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   def index
+    @product = Product.first #test de cloudinary (à dégager)
   end
 
   def create
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,8 +1,10 @@
 class Shop < ApplicationRecord
   belongs_to :user
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
   has_many :time_tables
   has_many :products, through: :stocks
+  has_one_attached :photo
+
 
   # validates address, presence: true
   # validates user_id, presence: true
