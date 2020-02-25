9d9be4542123c7dacfaf58f310e9e8aca72f322d
Tchudo <gael.lebreton@gmail.com>
2020-02-25 15:41:16 +0100

    bug resolved I hope

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_25_131719) do
-# ActiveRecord::Schema.define(version: 2020_02_25_132459) do
-
+ActiveRecord::Schema.define(version: 2020_02_25_132459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
