4633be58f8f3cbadb40e40fca076719ce0083053
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-26 17:23:47 +0100

    iti ok

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,6 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+// var MapboxDirections = require('@mapbox/mapbox-gl-directions');
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -28,6 +30,15 @@ const initMapbox = () => {
   }
 };
 
+
+
+/////////////////////////////////////////////////
+/////////////////// 2ème map ///////////////////
+////////////////////////////////////////////////
+
+
+
+
 const initMapbox2 = () => {
   const mapElement = document.getElementById('map-iti');
   const fitMapToMarkers = (map, markers) => {
@@ -38,37 +49,68 @@ const initMapbox2 = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
+    ///map
     const map = new mapboxgl.Map({
       container: 'map-iti',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v10',
+      zoom: 9
     });
 
+    //markers
     const markers = JSON.parse(mapElement.dataset.markers);
-    console.log(markers);
     markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // add this
-        .addTo(map);
+        // .addTo(map);
     });
 
-    fitMapToMarkers(map, markers);
+    //MapboxDirection
+    const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: 'metric',
+        profile: 'mapbox/walking',
+        interactive: false,
+        controls: {
+          inputs: false,
+          instructions: false,
+          profileSwitcher: false
+        }
+      });
 
-  // map.addControl(
-  //   new mapboxgl.GeolocateControl({
-  //   positionOptions: {
-  //     enableHighAccuracy: true
-  //   },
-  //   trackUserLocation: true
-  //   }),
-  // 'bottom-left'
-  // );
+    map.on('load', function () {
+
+      //Création itinéraire
+      const itineraire = () => {
+
+        navigator.geolocation.getCurrentPosition((data) => {
+
+          const lat = data.coords.latitude;
+          const long = data.coords.longitude;
+          const latDest = markers[0].lat ;
+          const longDest = markers[0].lng ;
+
+          // const createMarker = new mapboxgl.Marker()
+          //   .setLngLat([long, lat])
+          //   .addTo(map);
+
+          directions.setOrigin([long, lat]);
+          directions.setDestination([longDest, latDest]);
+          let bound = new mapboxgl.LngLatBounds();
+          bound.extend([long, lat]);
+          bound.extend([longDest, latDest]);
+          map.fitBounds(bound, {padding: 70, maxZoom: 15, duration: 100});
+        });
+      };
+      itineraire();
+
+    });
+    // fitMapToMarkers(map, markers);
+
+    map.addControl(directions, 'bottom-left');
   }
 
-navigator.geolocation.getCurrentPosition((data) => {
-  console.log(data);
-});
+
 
 };
 
