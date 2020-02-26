3e40cdf835b5a7e46f27d3f32c2afd74c83284bb
Tchudo <gael.lebreton@gmail.com>
2020-02-26 16:41:25 +0100

    installation d'ES7.6 et Searchkick4.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -161,7 +161,6 @@ puts "Product tag created ok"
 puts "Load image"
 
 file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-puts file
 p1.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "Image loaded OK"
 
