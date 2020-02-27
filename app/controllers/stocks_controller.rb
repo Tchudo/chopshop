18b45782bfc43621fbfc6937fb8d780795aa29ee
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-27 18:24:56 +0100

    infos map show ok

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,33 @@
 class StocksController < ApplicationController
   def index
     #@products = Product.search(params[:query]) "Elastic"
-    @products = Product.all #'sans Elastic'
-    @products.each do |product|
-      @stocks = Stock.where(product_id: product.id)
-      @markers = @stocks.map do |stock|
-        {
-        lat: stock.shop.latitude,
-        lng: stock.shop.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { shop: stock.shop , stock: stock}),
-        image_url: helpers.asset_url('lily.png')
-      }
-      end
+    # @products = Product.all #'sans Elastic'
+    # @products.each do |product|
+    #   @stocks = Stock.where(product_id: product.id)
+    #   @markers = @stocks.map do |stock|
+    #     {
+    #     lat: stock.shop.latitude,
+    #     lng: stock.shop.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { shop: stock.shop , stock: stock}),
+    #     image_url: helpers.asset_url('lily.png')
+    #   }
+    #   end
+    # end
+
+    @stocks = Stock.all
+    shops_address = []
+    @stocks.each do |stock|
+      shops_address << stock.shop
     end
+    @markers = shops_address.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { shop: shop , stock: @stocks.find(shop.id)}),
+        image_url: helpers.asset_url('lily.png'),
+      }
+    end
+
 
   end
 
