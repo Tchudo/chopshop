9724ddf530d1e98bc7fc2548634c8a11d3063751
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-02-28 13:28:38 +0100

    index favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,19 @@ class FavoritesController < ApplicationController
     @favorites = Favorite.all
   end
 
+  # def new
+  #   @favorite = Favorite.new
+  # end
+
   def create
+    raise
+    @favorite = Favorite.new(user_id: current_user[:id], product_id: params[:product_id])
+    # @favorite.user_id = current_user[:id]
+    # @favorite.product_id = params[:id]
+
+    if @favorite.save!
+      redirect_to stock_path(params[:id])
+    end
   end
 
   def destroy
