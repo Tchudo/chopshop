132c960bd8e06f1f5569e3be235dba82875bcfec
charlotte dezanneau <charlotte.dezanneau@hotmail.fr>
2020-03-01 10:47:02 +0100

    extraction infos ok

---
+++ app/javascript/shared/animation.js
@@ -0,0 +1,15 @@
+const change = () => {
+    document.getElementById('lily').classList.toggle("lilyb");
+  };
+
+
+const changeTime = () => {
+    setInterval(
+    () => {
+      change();
+    },
+    3000
+  );
+};
+
+export { changeTime }
